d03df37a19c0045ba6b695676c2b88da065efc5a
Marcos Cortez <42418437+marcosnsc@users.noreply.github.com>
2023-09-10 20:17:18 -0300

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../logo.svg';
 import './navbar.css';
+import react, { useState } from 'react';
 
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -10,7 +11,7 @@ const Navbar = () => {
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
-          <img src={logo} />
+          <img src={logo} alt="logo"/>
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
